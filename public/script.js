bf0462c37446f304117bf2f375dae68d549c4d8e
unknown <abhishekanantharam123@gmail.com>
2021-02-25 20:32:37 +0530

    peerjs integrate

--- public/script.js
+++ public/script.js
@@ -16,40 +16,41 @@ let peer = new Peer(undefined,{
 
 navigator.mediaDevices.getUserMedia({
     video:true,
-    audio:true
+    audio:false
 }).then(stream=>{
     myVideoStream = stream;
     addVideoStream(myVideo,myVideoStream);
-    videoGrid.append(myVideo);
+    peer.on('call',call=>{
+        call.answer(stream);
+        const video = document.createElement('video');
+        call.on('stream',userVideoStream=>{
+            addVideoStream(video,userVideoStream);
+        })
+    })
+    
+    //videoGrid.append(myVideo);
 
     socket.on('user-connected',(userId)=>{
-        connectToNewUser(userId,stream);
+        connectToNewUser(userId,myVideoStream);
     })
 
 })
-  .catch(err=>{
-      console.log(err);
-  })
+  
 
-
+//whenever connected to peer server hence first this occurs
 peer.on('open',id=>{
     socket.emit('join-room',ROOM_ID,id);
 })
 
 
-peer.on('call',call=>{
-    call.answer(stream);
-    const video = document.createElement('video');
-    call.on('stream',userVideoStream=>{
-        addVideoStream(video,userVideoStream);
-    })
-})
+
 
 
 const connectToNewUser = (userId,stream)=>{
     console.log(userId);
     const call  = peer.call(userId,stream);
     const video = document.createElement('video');
+    //add the received video stream
     call.on('stream',userVideoStream=>{
         addVideoStream(video,userVideoStream);
     })
@@ -65,5 +66,6 @@ const addVideoStream = (video,stream)=>{
     video.addEventListener('loadedmetadata',()=>{
         video.play();
     })
+    videoGrid.append(video);
     
 }
